6a2adf72b7dff0235716e1de751406dfacd06ae4
Alexander Gutev <alex.gutev@gmail.com>
2024-02-04 18:34:39 +0100

    Commit cell_state.cpp.

---
+++ src/cell_state.cpp
@@ -0,0 +1,48 @@
+#include "cell_state.hpp"
+
+live_cells::state_manager live_cells::state_manager::instance_ = {};
+
+live_cells::cell_state::~cell_state() noexcept  {
+    state_manager::global().remove(key_);
+}
+
+void live_cells::cell_state::add_observer(observer::ref o) {
+    if (observers.empty()) {
+        init();
+    }
+
+    auto it = observers.find(o);
+
+    if (it != observers.end()) {
+        it->second++;
+    }
+    else {
+        observers.emplace(o, 1);
+    }
+}
+
+void live_cells::cell_state::remove_observer(observer::ref o) {
+    auto it = observers.find(o);
+
+    if (it != observers.end()) {
+        if ((it->second -= 1) == 0) {
+            observers.erase(it);
+
+            if (observers.empty()) {
+                pause();
+            }
+        }
+    }
+}
+
+void live_cells::cell_state::notify_will_update() {
+    for (auto entry : observers) {
+        entry.first->will_update(key_);
+    }
+}
+
+void live_cells::cell_state::notify_update() {
+    for (auto entry : observers) {
+        entry.first->update(key_);
+    }
+}
