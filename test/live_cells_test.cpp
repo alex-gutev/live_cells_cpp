c4340c6493559fcc972949275d672cb1b6f2a2e2
Alexander Gutev <alex.gutev@gmail.com>
2024-02-11 22:04:59 +0100

    Add tests for equality cell key comparison.

--- test/live_cells_test.cpp
+++ test/live_cells_test.cpp
@@ -429,5 +429,31 @@ BOOST_AUTO_TEST_CASE(inequality_cell_observers_notified_on_2nd_arg_change) {
     BOOST_CHECK_EQUAL(observer->notify_count, 1);
 }
 
+BOOST_AUTO_TEST_CASE(equality_cells_compare_equal_if_same_arguments) {
+    auto a = live_cells::variable(1);
+    auto b = live_cells::variable(2);
+
+    live_cells::observable_ref eq1(a == b);
+    live_cells::observable_ref eq2(a == b);
+
+    std::hash<live_cells::observable_ref> hash;
+
+    BOOST_CHECK(eq1 == eq2);
+    BOOST_CHECK(!(eq1 != eq2));
+    BOOST_CHECK(hash(eq1) == hash(eq2));
+}
+
+BOOST_AUTO_TEST_CASE(equality_cells_compare_not_equal_if_different_arguments) {
+    auto a = live_cells::variable(1);
+    auto b = live_cells::variable(2);
+
+    live_cells::observable_ref eq1(a == b);
+    live_cells::observable_ref eq2(a == live_cells::value_cell(2));
+    live_cells::observable_ref eq3(live_cells::value_cell(2) == b);
+
+    BOOST_CHECK(eq1 != eq2);
+    BOOST_CHECK(eq1 != eq3);
+    BOOST_CHECK(!(eq1 == eq2));
+}
 
 BOOST_AUTO_TEST_SUITE_END()
