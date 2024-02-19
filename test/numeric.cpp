f631ec6781fe6a91a8524dccc0315f7c090b5f00
Alexander Gutev <alex.gutev@gmail.com>
2024-02-19 17:51:58 +0100

    Add unit tests for numeric operator overloads.

---
+++ test/numeric.cpp
@@ -0,0 +1,116 @@
+#define BOOST_TEST_DYN_LINK
+
+#include <string>
+#include <memory>
+#include <vector>
+#include <exception>
+
+#include <boost/test/unit_test.hpp>
+
+#include "constant_cell.hpp"
+#include "mutable_cell.hpp"
+#include "numeric.hpp"
+
+#include "test_util.hpp"
+
+
+BOOST_AUTO_TEST_SUITE(numeric_operators)
+
+BOOST_AUTO_TEST_CASE(additon) {
+    auto a = live_cells::value_cell(5);
+    auto b = live_cells::value_cell(6);
+
+    auto c = a + b;
+
+    BOOST_CHECK_EQUAL(c.value(), 11);
+}
+
+BOOST_AUTO_TEST_CASE(subtraction) {
+    auto a = live_cells::value_cell(13);
+    auto b = live_cells::value_cell(20);
+
+    auto c = a - b;
+
+    BOOST_CHECK_EQUAL(c.value(), -7);
+}
+
+BOOST_AUTO_TEST_CASE(multiplication) {
+    auto a = live_cells::value_cell(10);
+    auto b = live_cells::value_cell(8);
+
+    auto c = a * b;
+
+    BOOST_CHECK_EQUAL(c.value(), 80);
+}
+
+BOOST_AUTO_TEST_CASE(division) {
+    auto a = live_cells::value_cell(8);
+    auto b = live_cells::value_cell(4);
+
+    auto c = a / b;
+
+    BOOST_CHECK_EQUAL(c.value(), 2);
+}
+
+BOOST_AUTO_TEST_CASE(modulu) {
+    auto a = live_cells::value_cell(17);
+    auto b = live_cells::value_cell(3);
+
+    auto c = a % b;
+
+    BOOST_CHECK_EQUAL(c.value(), 2);
+}
+
+BOOST_AUTO_TEST_CASE(less_than) {
+    auto a = live_cells::value_cell(3);
+    auto b = live_cells::value_cell(8);
+
+    auto lt = a < b;
+    auto gt = b < a;
+    auto eq = a < a;
+
+    BOOST_CHECK(lt.value());
+    BOOST_CHECK(!gt.value());
+    BOOST_CHECK(!eq.value());
+}
+
+BOOST_AUTO_TEST_CASE(less_than_or_equal) {
+    auto a = live_cells::value_cell(3);
+    auto b = live_cells::value_cell(8);
+
+    auto lt = a <= b;
+    auto gt = b <= a;
+    auto eq = a <= a;
+
+    BOOST_CHECK(lt.value());
+    BOOST_CHECK(!gt.value());
+    BOOST_CHECK(eq.value());
+}
+
+BOOST_AUTO_TEST_CASE(greater_than) {
+    auto a = live_cells::value_cell(3);
+    auto b = live_cells::value_cell(8);
+
+    auto gt = a > b;
+    auto lt = b > a;
+    auto eq = a > a;
+
+    BOOST_CHECK(!gt.value());
+    BOOST_CHECK(lt.value());
+    BOOST_CHECK(!eq.value());
+}
+
+BOOST_AUTO_TEST_CASE(greater_than_or_equal) {
+    auto a = live_cells::value_cell(3);
+    auto b = live_cells::value_cell(8);
+
+    auto gt = a >= b;
+    auto lt = b >= a;
+    auto eq = a >= a;
+
+    BOOST_CHECK(!gt.value());
+    BOOST_CHECK(lt.value());
+    BOOST_CHECK(eq.value());
+}
+
+BOOST_AUTO_TEST_SUITE_END()
